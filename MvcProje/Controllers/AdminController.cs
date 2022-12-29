b03b8e22476f225373496ab3a8f25c15a8b2daf0
Oguzhan-Koca <83463216+Oguzhan-Koca@users.noreply.github.com>
2022-12-29 20:16:42 +0300

    genel değişiklik
    apiden dolaylı sıkıntılardan dolayı api iptal

---
+++ MvcProje/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MvcProje.Models.Entity;
+using MvcProje.Repositories;
+
+namespace MvcProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        GenericRepository<Animal> repository = new GenericRepository<Animal>();
+        //[Authorize]
+        public IActionResult Index()
+        {
+            var animals = repository.List();
+            return View(animals);
+        }
+        [HttpGet]
+        public IActionResult AnimalAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AnimalAdd(Animal p)
+        {
+            p.AnimalStatus = true;
+            p.UserID = 15;
+            repository.TAdd(p);
+            return RedirectToAction("Index"); //admin controller daki index metoduna gönderme yani yukarı
+        }
+        public IActionResult AnimalDelete(int id)
+        {
+            Animal a=repository.Find(x=> x.AnimalID==id);
+            repository.TDelete(a);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AnimalUpdate(int id)
+        {
+            Animal a=repository.Find(x=>x.AnimalID==0); 
+            return View(a);
+
+        }
+
+    }
+}
--- WebProje/WebProje/Models/Entities/Yeni.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebProje.Models.Entities
-{
-    public class Yeni
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
